312eee14aaec6f1b40390bd66a1e185fd6883072
Simon <120012623+simonh35@users.noreply.github.com>
2025-01-24 17:37:09 +0100

    Add files via upload

--- kms.py
+++ kms.py
@@ -1,5 +1,6 @@
 import argparse
-from langchain_community.document_loaders import PyPDFLoader
+import PyPDF2
+from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
 from populate_database import clear_database, add_to_chroma
 from pdf_from_zotero import fetch_all_items, download_content, check_zotero_connection
 from splitter import split_documents
@@ -10,7 +11,7 @@ import os
 DATA_PATH = "/home/t35/kms/data/"
 
 # Dictionary to store the metadata of the documents.
-docs = {}
+zotero_metadata = {}
 
 # Clear database if requested.
 parser = argparse.ArgumentParser()
@@ -26,31 +27,25 @@ if args.reset:
 # check_zotero_connection()
 
 # Fetch all items from Zotero
-# docs = fetch_all_items()
-# for key, title in docs.items():
+# zotero_metadata = fetch_all_items()
+# for key, title in zotero_metadata.items():
 #     print(f"Key: {key} - Title: {title}")
 
 # Download items from Zotero
-# for key, title in docs.items():
+# for key, title in zotero_metadata.items():
 #     print(f"Downloading {title} ...")
 #     download_content(key,title, DATA_PATH)
 
 # Load Docs from DATA_PATH into data 
-data = []
-pages = []
-chunks = []
+data = {}
 
+# Load the PDF files into the data dictionary.
 for filename in os.listdir(DATA_PATH):
     if(filename.endswith(".pdf")):
-        file_loader = PyPDFLoader(os.path.join(DATA_PATH, f"{filename}.pdf"))
-        data.append(file_loader)
+        loader = PyPDFLoader(f"{DATA_PATH+filename}")
+        doc = loader.load()
+        chunks = split_documents(doc)
+        # data[filename] = doc
+        print(f"Split {filename} into {len(chunks)} chunks.")
+        add_to_chroma(chunks)
 
-# Create (or update) the data store.
-for doc in data:
-    for page in doc.pages:
-        print(page)
-#         pages.append(page)
-    
-#     chunks = (split_documents(pages))
-#     print(f"Split {doc.get("title")} into {len(chunks)} chunks.")
-    #add_to_chroma(chunks)
